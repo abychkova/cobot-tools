5c5d4afc9f59332bbeaef9dd5e2a33031faa2b86
abychkova <gromovaaa@biocad.ru>
2020-01-13 14:23:21 +0300

    weighted random

--- src/Bio/Tools/Sequence/OligoDesigner/Utils.hs
+++ src/Bio/Tools/Sequence/OligoDesigner/Utils.hs
@@ -1,11 +1,21 @@
 module Bio.Tools.Sequence.OligoDesigner.Utils
  (translateDNA
  ,assemble
+ ,weightedRandom
+ ,randomCodon
  ) where
 
 import Bio.NucleicAcid.Nucleotide.Type (DNA(..))
 import Bio.Tools.Sequence.OligoDesigner.Types (Olig(..), OligSet(..))
+import Bio.Tools.Sequence.CodonOptimization.Constants (codonFrequencies, ak2Codon)
 import Debug.Trace (trace)
+import Bio.Protein.AminoAcid.Type (AA)
+import Bio.Tools.Sequence.CodonOptimization.Types (Organism)
+import Data.Maybe (fromMaybe)
+import Data.Map as Map (lookup)
+import System.Random (randomRIO)
+import Debug.Trace (trace)
+import Data.List (sortOn)
 
 translateDNA :: [DNA] -> [DNA]
 translateDNA = map translate
@@ -24,3 +34,19 @@ assemble (OligSet fwd rvd) = constract fwd rvd 0 [] where
     constract (Olig seq1 startLeft endLeft : xs) (Olig seq2 startRight endRight : ys) prevEnd acc = constract xs ys endRight res
       where
         res = acc ++ drop (prevEnd - startLeft) seq1 ++ translateDNA (drop (endLeft - startRight) seq2)
+
+randomCodon :: Organism -> AA -> IO [DNA]
+randomCodon org aa = weightedRandom codonToWeight where
+    codons = fromMaybe [] (Map.lookup aa ak2Codon)
+    codonToWeight = map (\codon -> (codon, codonWeight codon)) codons
+
+    codonWeight :: [DNA] -> Double
+    codonWeight codon = fromMaybe 0 (Map.lookup codon (codonFrequencies org))
+
+weightedRandom :: Show a => [(a, Double)] -> IO a
+weightedRandom items = do
+    random <- randomRIO (0, sum $ map snd items)
+    return $ getWeightedItem random 0 (sortOn snd items)
+  where
+    getWeightedItem :: Show a => Double -> Double -> [(a, Double)] -> a
+    getWeightedItem randomValue acc ((x, w) : xs) = if acc + w >= randomValue then x else getWeightedItem randomValue (acc + w) xs
--- test/OligoDesigner/SpecUtils.hs
+++ test/OligoDesigner/SpecUtils.hs
@@ -3,22 +3,26 @@
 module OligoDesigner.SpecUtils where
 
 import Bio.NucleicAcid.Nucleotide (DNA)
-import Bio.Tools.Sequence.OligoDesigner.Types (Olig(..), OligSet(..))
-import Bio.Tools.Sequence.OligoDesigner.Utils (assemble)
 import Bio.Tools.Sequence.OligoDesigner.Algo (generateOligs)
-import Test.Hspec (Spec, describe, it, shouldBe)
+import Bio.Tools.Sequence.OligoDesigner.Types (Olig(..), OligSet(..))
+import Bio.Tools.Sequence.OligoDesigner.Utils (assemble, weightedRandom)
 import Debug.Trace (trace)
+import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
 
 utilsSpec :: Spec
-utilsSpec = describe "utilsSpec" $ do
-    assembleSpec
-    assembleWithGapSpec
+utilsSpec =
+    describe "utilsSpec" $ do
+        assembleSpec
+        assembleWithGapSpec
+        weightedRandomSimpleSpec
+        weightedRandomSpec
+        weightedRandomAllZeroSpec
+        weightedRandomForUnsortedSpec
 
 assembleSpec :: Spec
 assembleSpec =
     describe "assembleSpec" $
-    it "assembleSpec" $
-     do
+    it "assembleSpec" $ do
         let fwd = [Olig "AA" 0 2, Olig "TT" 2 4, Olig "GG" 4 6, Olig "CC" 6 8, Olig "AA" 8 10]
         let rsd = [Olig "TA" 1 3, Olig "AC" 3 5, Olig "CG" 5 7, Olig "GT" 7 9, Olig "TA" 9 11]
         let oligs = OligSet fwd rsd
@@ -27,9 +31,50 @@ assembleSpec =
 assembleWithGapSpec :: Spec
 assembleWithGapSpec =
     describe "assembleWithGapSpec" $
-    it "assembleWithGapSpec" $
-     do
+    it "assembleWithGapSpec" $ do
         let fwd = [Olig "AATT" 0 4, Olig "CCAA" 6 10]
         let rsd = [Olig "ACCG" 3 7, Olig "TTA" 8 11]
         let oligs = OligSet fwd rsd
         assemble oligs `shouldBe` "AATTGGCCAAT"
+
+weightedRandomSimpleSpec :: Spec
+weightedRandomSimpleSpec =
+    describe "weightedRandomSimpleSpec" $
+    it "should return weigthed random element" $ do
+        res <- weightedRandom [(1, 1), (2, 0)]
+        res `shouldBe` 1
+        res2 <- weightedRandom [(1, 0.99999999), (2, 0.00000001)]
+        res2 `shouldBe` 1
+        res3 <- weightedRandom [(1, 0), (2, 1)]
+        res3 `shouldBe` 2
+
+weightedRandomSpec :: Spec
+weightedRandomSpec =
+    describe "weightedRandomSpec" $
+    it "should return weigthed random element" $ do
+        res <- sequence [weightedRandom [(3, 7), (4, 300), (1, 1600)] | x <- [0..10]]
+        let count1 = length $ filter (== 1) res
+        let count3 = length $ filter (== 3) res
+        let count4 = length $ filter (== 4) res
+        count1 `shouldSatisfy` (> count3)
+        count1 `shouldSatisfy` (> count4)
+        count4 `shouldSatisfy` (>= count3)
+
+weightedRandomForUnsortedSpec :: Spec
+weightedRandomForUnsortedSpec =
+    describe "weightedRandomForUnsortedSpec" $
+    it "should return weigthed random element" $ do
+        res <- sequence [weightedRandom [(4, 300), (3, 7), (1, 1600)] | x <- [0..10]]
+        let count1 = length $ filter (== 1) res
+        let count3 = length $ filter (== 3) res
+        let count4 = length $ filter (== 4) res
+        count1 `shouldSatisfy` (> count3)
+        count1 `shouldSatisfy` (> count4)
+        count4 `shouldSatisfy` (>= count3)
+
+weightedRandomAllZeroSpec :: Spec
+weightedRandomAllZeroSpec =
+    describe "weightedRandomAllZeroSpec" $
+    it "should return weigthed random element" $ do
+        res <- weightedRandom [(1, 0), (2, 0)]
+        res `shouldBe` 1
